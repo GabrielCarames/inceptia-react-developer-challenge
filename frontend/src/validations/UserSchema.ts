1ffd2f277bd1bea5b83cbd7a61617bd5acee5649
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-18 11:25:39 -0300

    feat: add zod UserSchema

---
+++ frontend/src/validations/UserSchema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+
+export const userSchema = z.object({
+  email: z
+    .string()
+    .email({
+      message: "El email debe ser válido"
+    })
+    .min(3, {
+      message: "El email debe contener al menos 3 caracteres."
+    })
+    .max(80, {
+      message: "El email debe contener menos de 80 caracteres."
+    }),
+  password: z
+    .string()
+    .min(5, {
+      message: "La contraseña debe tener al menos 5 caracteres."
+    })
+    .max(20, {
+      message: "La contraseña debe tener hasta 20 caracteres."
+    })
+})
