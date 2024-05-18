c5db745d8396e95bf6489057f7c68616e2fb64e7
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-18 09:15:24 -0300

    feat: add color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   darkMode: "selector",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        dark: "#0E1217",
+        shark: "#1C1F26",
+        "rock-blue": "#9EA9C3",
+        manatee: "#828BA2",
+        tuna: "#2D323A",
+        "ghost-white": "#F5F8FC",
+        white: "#FFFFFF",
+        "white-smoke": "#F1F2F3",
+        "white-gallery": "#E9EAEB",
+        raven: "#767B87",
+        "persian-green": "#01A59E",
+        "monte-carlo": "#7ECFCB"
+      }
+    }
   },
   plugins: []
 }
