ef16e8e86ced527556080c806b5cd72730ad9e97
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-19 17:54:56 -0300

    feat(Client): add navigation on click

--- frontend/src/components/Client.tsx
+++ frontend/src/components/Client.tsx
@@ -1,8 +1,21 @@
-export default function Client({ name }: { name: string }) {
+import { Link } from "react-router-dom"
+
+export default function Client({
+  clientId,
+  name
+}: {
+  clientId: number
+  name: string
+}) {
   return (
-    <li className="flex items-center gap-2">
-      <span className="icon-[ic--outline-account-circle] size-8" />
-      <p>{name}</p>
+    <li>
+      <Link
+        to={`/admin/client/${clientId}`}
+        className="flex items-center gap-2"
+      >
+        <span className="icon-[ic--outline-account-circle] size-8" />
+        <p>{name}</p>
+      </Link>
     </li>
   )
 }
