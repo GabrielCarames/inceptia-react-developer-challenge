cf1dcf02a4e521dd1bf5060b9b0bd5cd65798611
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-21 08:57:24 -0300

    refactor(TableSearch): add select options

--- frontend/src/components/TableSearch.tsx
+++ frontend/src/components/TableSearch.tsx
@@ -3,9 +3,9 @@ import { TableSearchProps } from "@/types/interfaces"
 const TableSearch = ({
   placeholder,
   search,
-  setSearch
-}: // setSortBy
-TableSearchProps) => {
+  setSearch,
+  setSearchBy
+}: TableSearchProps) => {
   return (
     <div className="relative flex h-14 w-full items-center border-stroke bg-gray-2 pl-4 text-white bg-shark border border-tuna border-b-transparent rounded-lg">
       <button className="absolute top-1/2 left-4 -translate-y-1/2">
@@ -18,22 +18,18 @@ TableSearchProps) => {
         value={search}
         onChange={e => setSearch(e.target.value)}
       />
-      {/* {setSortBy && (
-        <select
-          name="order"
-          className="mr-2 w-full max-w-[200px] border border-stroke border-rock-blue rounded-lg text-white bg-tuna p-2"
-          onChange={e => setSortBy(e.target.value)}
-          defaultValue={"caseId"}
-        >
-          <option value="caseId">Buscar por ID Caso</option>
-          <option value="phone">Buscar por Teléfono</option>
-          <option value="dni">Buscar por DNI</option>
-          <option value="group">Buscar por Grupo</option>
-          <option value="order">Buscar por Orden</option>
-          <option value="call">Buscar por Llamada</option>
-          <option value="state">Buscar por Estado</option>
-        </select>
-      )} */}
+      <select
+        name="order"
+        className="mr-2 w-full max-w-[200px] border border-stroke border-rock-blue rounded-lg text-white bg-tuna p-2"
+        onChange={e => setSearchBy(e.target.value)}
+        defaultValue={"last_updated"}
+      >
+        <option value="last_updated">Buscar por Gestionado</option>
+        <option value="case_uuid">Buscar por ID Caso</option>
+        <option value="phone">Buscar por Teléfono</option>
+        <option value="dni">Buscar por DNI</option>
+        <option value="case_result name">Buscar por Estado</option>
+      </select>
     </div>
   )
 }
