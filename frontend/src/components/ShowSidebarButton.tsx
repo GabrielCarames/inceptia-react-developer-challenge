fcebae139e03cb41c8e4eb56cfc8dc075e792afc
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-22 14:30:54 -0300

    feat: add ShowSidebarButton

---
+++ frontend/src/components/ShowSidebarButton.tsx
@@ -0,0 +1,24 @@
+import { ShowSidebarButtonProps } from "@/types/interfaces"
+
+export default function ShowSidebarButton({
+  openSidebar,
+  setOpenSidebar,
+  className
+}: ShowSidebarButtonProps) {
+  return (
+    <button
+      className={`flex justify-center items-center border border-persian-green rounded-lg ${
+        className ? className : ""
+      }`}
+      onClick={() => setOpenSidebar(!openSidebar)}
+    >
+      <span
+        className={`${
+          openSidebar
+            ? "icon-[ic--baseline-keyboard-double-arrow-left]"
+            : "icon-[ic--baseline-keyboard-double-arrow-right]"
+        } size-7`}
+      />
+    </button>
+  )
+}
