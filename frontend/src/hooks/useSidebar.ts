ddea877bc4adabf9cae8d816ce041437b415034a
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-22 14:31:00 -0300

    feat: add useSidebar hook

---
+++ frontend/src/hooks/useSidebar.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+const useSidebar = () => {
+  const [openSidebar, setOpenSidebar] = useState(true)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1024) {
+        setOpenSidebar(false)
+      } else {
+        setOpenSidebar(true)
+      }
+    }
+
+    window.addEventListener("resize", handleResize)
+    handleResize()
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
+
+  return { openSidebar, setOpenSidebar }
+}
+
+export default useSidebar
